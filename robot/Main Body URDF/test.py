6a9de7f5613ed79798c4b70008f1fb8f0efd5aaa
eminalparslan <eminalparslan@gmail.com>
2022-02-06 21:06:04 -0600

    reorganization pt2 + README

--- robot/Main Body URDF/test.py
+++ robot/Main Body URDF/test.py
@@ -1,10 +1,17 @@
 import pybullet as p
 import pybullet_data
+
+# Create a client of pybullet physics server
 client = p.connect(p.GUI)
+
 p.setGravity(0, 0, -10, physicsClientId=client)
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-planeId = p.loadURDF(r"plane.urdf")
-robotId = p.loadURDF(r"urdf/Main Body URDF.urdf", basePosition=[0,0,0.5])
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("gray.urdf", basePosition=[0,0,0.5])
+
 position, orientation = p.getBasePositionAndOrientation(robotId)
+
+# Step through simulation (each step is 1/240 of a second)
 while True: 
     p.stepSimulation()
